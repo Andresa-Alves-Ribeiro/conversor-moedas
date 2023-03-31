afa8905415a9773c0eb6173a2939fdca3faa2193
Andresa Alves <andresa_15ga@hotmail.com>
2023-03-30 21:34:51 -0300

    consertando problema de conversão

--- src/components/conversor/conversor.jsx
+++ src/components/conversor/conversor.jsx
@@ -1,58 +1,56 @@
 import './styles.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Exchange from "../../assets/exchange.svg";
 import { Chart } from '../chart/chart';
+import axios from 'axios';
+
+function getCurrencySymbol(currencyCode) {
+    switch (currencyCode) {
+        case 'BRL':
+            return 'R$';
+        case 'USD':
+            return '$';
+        case 'EUR':
+            return '€';
+        case 'GBP':
+            return '£';
+        default:
+            return '';
+    }
+}
 
 export default function Conversor() {
-    const [amount, setAmount] = useState("$1.00");
-    const [fromCurrency, setFromCurrency] = useState("US");
-    const [toCurrency, setToCurrency] = useState("BR");
-    const [currencySymbol, setCurrencySymbol] = useState("R$");
+    const [amount, setAmount] = useState(1);
+    const [fromCurrency, setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState('BRL');
+    const [result, setResult] = useState(0);
 
-    const handleAmountChange = (event) => {
-        setAmount(event.target.value);
+    const convertCurrency = async () => {
+        const response = await axios.get(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`);
+        const exchangeRate = response.data.rates[toCurrency];
+        setResult(amount * exchangeRate);
     };
 
-    const handleFromCurrencyChange = (event) => {
-        setFromCurrency(event.target.value);
-    };
+    useEffect(() => {
+        convertCurrency();
+    }, [fromCurrency, toCurrency, amount]);
 
-    const handleToCurrencyChange = (event) => {
-        setToCurrency(event.target.value);
+    const handleAmountChange = (e) => {
+        setAmount(e.target.value);
     };
 
     const handleExchangeClick = () => {
-        const from = fromCurrency;
-        const to = toCurrency;
-    
-        setFromCurrency(to);
-        setToCurrency(from);
-    
-        const numericValue = parseFloat(amount.replace(/[^0-9.-]+/g, ""));
-        let newNumericValue = numericValue;
-    
-        if (from === 'USD') {
-            newNumericValue = numericValue * 0.19;
-        } else if (from === 'EUR') {
-            newNumericValue = numericValue * 6.164;
-        } else if (from === 'GBP') {
-            newNumericValue = numericValue * 7.119;
-        }
-    
-        if (to === 'USD') {
-            newNumericValue /= 5.17;
-        } else if (to === 'EUR') {
-            newNumericValue /= 6.164;
-        } else if (to === 'GBP') {
-            newNumericValue /= 7.119;
-        }
-    
-        const newAmount = newNumericValue.toLocaleString('pt-br', { style: 'currency', currency: to === 'BR' ? 'BRL' : 'USD' });
-        setAmount(newAmount);
-
-        setCurrencySymbol(to !== 'BR' ? 'R$' : 'US$');
+        setFromCurrency(toCurrency);
+        setToCurrency(fromCurrency);
     };
-    
+
+    function handleSwapCurrencies() {
+        setFromCurrency(toCurrency);
+        setToCurrency(fromCurrency);
+    }
+
+    const fromCurrencySymbol = getCurrencySymbol(fromCurrency);
+    const toCurrencySymbol = getCurrencySymbol(toCurrency);
 
     return (
         <main>
@@ -63,7 +61,7 @@ export default function Conversor() {
                         <input
                             type="text"
                             className="amount"
-                            value={amount}
+                            value={`${amount}`}
                             onChange={handleAmountChange}
                         />
 
@@ -103,7 +101,7 @@ export default function Conversor() {
                             readOnly
                             type="text"
                             className="amount"
-                            value={`${currencySymbol} ${5.17 * parseFloat(amount.replace(/[^0-9.-]+/g, ""))}`}
+                            value={`${toCurrencySymbol} ${result}`}
                         />
 
                         <div className="dropdown">
@@ -145,4 +143,5 @@ export default function Conversor() {
             </section>
         </main>
     );
+
 }
